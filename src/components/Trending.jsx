2ae602d0597ef3e24af41cdf31703c15372bf789
Kayc <kaycmendes@gmail.com>
2020-08-21 17:57:58 -0300

    add featured gifs

---
+++ src/components/Trending.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Gif from './Gif';
+
+const TrendingGifs = props => {
+
+  const trendResults = props.data;
+  let gifs = trendResults.map(gif =>
+    <Gif 
+    url={gif.images.fixed_height.url}
+    key={gif.id}
+     />
+  )
+
+  return (
+    <ul className="gif-list">
+      {gifs}
+    </ul>
+  );
+}
+
+export default TrendingGifs;
