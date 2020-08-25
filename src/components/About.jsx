5af61fca9b3b5b8de0ded667ea315b1724740112
Kayc <kaycmendes@gmail.com>
2020-08-25 02:04:03 -0300

    add about page

---
+++ src/components/About.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Header from './Header'
+
+class About extends Component {
+    render() {
+        return (
+            <>
+                <Header />
+                <div className="about-wrapper">
+                    <article className="">
+                    <h2>What is gifLoop?</h2>
+                        <p> It's a gif searcher that retrieve only gifs that loops</p>
+                    </article>
+                    
+                    <article className="how"> 
+                    <h2>How was it made?</h2>
+                    <p>The frontend was built using ReactJS for the components, bootstrap-react for the style plus some CSS customizations and react-router to handle the links. </p> 
+                    <p>For the backend a simple API call using axios library to fetch data from the giphy endpoint API.</p> 
+                    </article>
+                    
+
+                </div>
+            </>
+        );
+    }
+}
+export default About;
