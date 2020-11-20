f52df2bb41d36e977511bed46040741bfc64a009
Kayc <kaycmendes@gmail.com>
2020-11-20 15:06:10 -0300

    copy gifs

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-const Gif = props => (
-  <li className="gif-wrap">
-    <img src={props.url} alt=""/>
-  </li>
-);
+class Gif extends React.Component {
+
+  render() {
+    return (
+      <li className="gif-wrap">
+        <img id={Math.round(Math.random() * (500 - 10) + 10)} onClick={this.handleClick} src={this.props.url} alt="" />
+      </li>
+    );
+  }
+
+  handleClick(e) {
+    let id = document.getElementById(e.target.id)
+    let link = id.src;
+    var dummy = document.createElement('input');  
+    document.body.appendChild(dummy);
+    dummy.value = link;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+
+  }
+
+}
 
 export default Gif;
+
